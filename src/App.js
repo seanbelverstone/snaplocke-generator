4540c471e5726572f6c21ef2a3f54df68fd1c948
Sean Belverstone <seanbelverstone@gmail.com>
2024-08-15 11:24:44 -0500

    instead of going by generation, it made more sense to go by game. I listed out all of the games and their possible expansions and mapped them out instead of writing them all manually. Also updated the check to prevent submit if no games are selected

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Button, Checkbox, FormControlLabel } from '@mui/material';
 import './App.css';
+import camelToTitle from './utils';
 
 function App() {
 
 	const [formData, setFormData] = useState({
-		generations: {
-			genOne: true,
-			genTwo: false,
-			genThree: false,
-			genFour: false,
-			genFive: false,
-			genSix: false,
-			genSeven: false,
-			genEight: false,
-			genNine: false,
-			all: false
+		version: {
+			genOne: {
+				red: true,
+				blue: false,
+				yellow: false
+			},
+			genTwo: {
+				gold: false,
+				silver: false,
+				crystal: false
+			},
+			genThree: {
+				ruby: false,
+				sapphire: false,
+				emerald: false,
+				fireRed: false,
+				leafGreen: false
+			},
+			genFour: {
+				diamond: false,
+				pearl: false,
+				platinum: false,
+				heartgold: false,
+				soulsilver: false
+			},
+			genFive: {
+				black: false,
+				white: false,
+				blackTwo: false,
+				whiteTwo: false
+			},
+			genSix: {
+				x: false,
+				y: false,
+				omegaRuby: false,
+				alphaSapphire: false
+			},
+			genSeven: {
+				sun: false,
+				moon: false,
+				ultraSun: false,
+				ultraMoon: false,
+				letsGoPikachu: false,
+				letsGoEevee: false
+			},
+			genEight: {
+				sword: false,
+				shield: false,
+				expansions: {
+					theIsleOfArmor: false,
+					theCrownTundra: false,
+				},
+				brilliantDiamond: false,
+				shiningPearl: false,
+				legendsArceus: false
+			},
+			genNine: {
+				scarlet: false,
+				violet: false,
+				expansions: {
+					theTealMask: false,
+					theIndigoDisk: false
+				}
+			}
 		},
 		noLegendaries: false
 	});
 
-	const getData = () => fetch('https://pokeapi.co/api/v2/pokemon?limit=2000')
+	const getData = () => fetch('https://pokeapi.co/api/v2/version?limit=50')
 		.then(response => response.json())
 		.then(data => {
 			return data;
@@ -28,44 +82,85 @@ function App() {
 	
 	getData();
 
-	const generate = () => {
-		// console.log(form);
+	const generate = (event) => {
+		event.preventDefault();
+		// if all is selected, get all pokemon
+		// else, get the generation's key and use that to 
 	};
 
-	const generationsEmpty = !(Object.values(formData.generations).some(datum => datum));
+	const areSelectionsEmpty = () => {
+		const flattenedFormData = Object.entries(formData.version);
+		return !flattenedFormData.some(([key, value]) => Object.values(value).some(item => typeof item === 'boolean' && item))
+	}
 
   return (
     <div className="page">
 			<h1>Snaplocke Generator</h1>
-			<div id="form">
-				<p>Generation</p>
+			<div id="form" onSubmit={generate}>
+				<p>Version</p>
+				{areSelectionsEmpty() && <p className="error">At least one game must be selected.</p>}
 				<form>
 					<div className="genGroup">
-						<div>
-							<FormControlLabel control={<Checkbox defaultChecked />} label="Gen 1" value={formData.genOne} onChange={e => setFormData({ ...formData, generations: { ...formData.generations, genOne: e.target.checked } })} />
-							<FormControlLabel control={<Checkbox />} label="Gen 2"  value={formData.genTwo} onChange={e => setFormData({ ...formData, generations: { ...formData.generations, genTwo: e.target.checked } })} />
-							<FormControlLabel control={<Checkbox />} label="Gen 3"  value={formData.genThree} onChange={e => setFormData({ ...formData, generations: { ...formData.generations, genThree: e.target.checked } })} />
+						{Object.entries(formData.version).map(([generationName, generationValue]) => (
+							<div key={generationName} className="generation">
+								<p className="genTitle">{camelToTitle(generationName)}</p>
+							{Object.entries(generationValue).map(([key, value]) => {
+								if (key !== 'expansions') {
+								  return (
+										<FormControlLabel
+											key={key}
+											control={<Checkbox defaultChecked={key === 'red'} />}
+											label={camelToTitle(key)}
+											value={formData.version[generationName][value]}
+											onChange={e => setFormData(
+												{ ...formData, version: {
+													...formData.version, [generationName]: {
+														...formData.version[generationName], [key]: e.target.checked
+													}
+												}
+											}
+											)} />)
+								}
+								return (
+									<div>
+										<span>Include Expansions?</span>
+										{Object.entries(value).map(([expName, expValue]) => {
+										if (expName !== 'expansions') {
+											return (
+											<FormControlLabel
+												key={expName}
+												control={<Checkbox />}
+												label={camelToTitle(expName)}
+												value={formData.version[generationName].expansions[expName]}
+												onChange={e => setFormData(
+													{ ...formData, version: {
+														...formData.version, [generationName]: {
+															...formData.version[generationName], expansions: {
+																...formData.version[generationName].expansions, [key]: e.target.checked
+															}
+														}
+													}
+												})} />
+										)
+								}
+								return <React.Fragment />
+							})}
+									</div>
+								)
+							}
+							)
+							}
 						</div>
-						<div>
-							<FormControlLabel control={<Checkbox />} label="Gen 4"  value={formData.genFour} onChange={e => setFormData({ ...formData, generations: { ...formData.generations, genFour: e.target.checked } })} />
-							<FormControlLabel control={<Checkbox />} label="Gen 5"  value={formData.genFive} onChange={e => setFormData({ ...formData, generations: { ...formData.generations, genFive: e.target.checked } })} />
-							<FormControlLabel control={<Checkbox />} label="Gen 6"  value={formData.genSix} onChange={e => setFormData({ ...formData, generations: { ...formData.generations, genSix: e.target.checked } })} />
-						</div>
-						<div>
-							<FormControlLabel control={<Checkbox />} label="Gen 7"  value={formData.genSeven} onChange={e => setFormData({ ...formData, generations: { ...formData.generations, genSeven: e.target.checked } })} />
-							<FormControlLabel control={<Checkbox />} label="Gen 8"  value={formData.genEight} onChange={e => setFormData({ ...formData, generations: { ...formData.generations, genEight: e.target.checked } })} />
-							<FormControlLabel control={<Checkbox />} label="Gen 9"  value={formData.genNine} onChange={e => setFormData({ ...formData, generations: { ...formData.generations, genNine: e.target.checked } })} />
-						</div>
-						<div>
-							<FormControlLabel control={<Checkbox />} label="All"  value={formData.all} onChange={e => setFormData({ ...formData, generations: { ...formData.generations, all: e.target.checked } })} />
-						</div>
-						{generationsEmpty && <p className="error">At least one generation must be selected.</p>}
+					))}
 					</div>
 					<div className="misc">
 						<FormControlLabel control={<Checkbox />} label="Ban Legendaries?" value={formData.noLegendaries} onChange={e => setFormData({ ...formData, noLegendaries: e.target.checked })} />
 					</div>
+					<Button
+						variant="outlined"
+						disabled={areSelectionsEmpty()}
+						type="submit">Generate</Button>
 				</form>
-				<Button variant="outlined" disabled={generationsEmpty} onClick={generate}>Generate</Button>
 			</div>
 		</div>
 
---
+++ src/utils.js
@@ -0,0 +1,6 @@
+function camelToTitle(string) {
+  const result = string.replace(/([A-Z])/g, ' $1');
+  return result.charAt(0).toUpperCase() + result.slice(1);
+}
+
+export default camelToTitle;
