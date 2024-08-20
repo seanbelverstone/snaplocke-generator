32ed30951af80ee370553f0b857f7a18cf1e3598
Sean Belverstone <seanbelverstone@gmail.com>
2024-08-19 22:07:42 -0500

    added a snap function

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { legendaries, pokemonPerVersion } from './gameData';
 import camelToTitle from './utils';
+import { Button } from '@mui/material';
+import snapImage from './assets/snap.png';
+import thanos from './assets/thanos.jpeg';
 
 /* this page will list out the pokemon in different layouts:
  - basic
@@ -14,8 +17,9 @@ import camelToTitle from './utils';
 function Results(props) {
 	const { submitted, version, noLegendaries, expansions } = props;
 	const [pokemon, setPokemon] = useState([]);
-	const [pokemonDetails, setPokemonDetails] = useState({});
+	const [pokemonDetails, setPokemonDetails] = useState([]);
 	const [dataComplete, setDataComplete] = useState(false);
+	const [snapped, setSnapped] = useState(false);
 	/* 
 		click a button to snap	
 		animation of snap - fade out half
@@ -23,6 +27,7 @@ function Results(props) {
 
 	useEffect(() => {
 		setDataComplete(false);
+		setSnapped(false);
 		getSprites();
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [submitted])
@@ -40,10 +45,39 @@ function Results(props) {
 			setDataComplete(true);
 	}
 
+	const snap = () => {
+		const newPokemonList = [ ...pokemonDetails ];
+		for (let i = newPokemonList.length; i >= (Math.ceil(pokemonDetails.length / 2)); i--) {
+			newPokemonList.splice(Math.floor(Math.random() * newPokemonList.length), 1);
+		}
+		console.log(pokemonDetails.length);
+		console.log(newPokemonList.length, newPokemonList);
+		setPokemonDetails(newPokemonList);
+		setSnapped(true);
+	};
+
   return (
-    <div className="results">
-			<h2>Results here!</h2>
-			<div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+    <div className="results" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+				<div style={{ width: '80%', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
+				<Button
+					variant="outlined"
+					onClick={snap}
+					style={{ width: '100%', display: 'flex', flexDirection: 'column' }}
+					disabled={snapped}
+					>
+						{snapped ? (
+							<>
+								<img src={thanos} alt="thanos" style={{ maxWidth: '192px' }} />
+								<span style={{ color: 'red' }}>I AM INEVITABLE</span>
+							</>
+						) : (
+							<>
+								<img src={snapImage} alt="The Infinity Gauntlet in a snapping pose." style={{ maxWidth: '192px' }} />
+								<span>REMOVE HALF</span>
+							</>
+						)}
+
+				</Button>
 				{dataComplete && pokemonDetails?.map(pokemon => (
 					<div id="card" key={pokemon.name}>
 						<img style={{ maxWidth: '96px', maxHeight: '96px', imageRendering: 'pixelated' }} src={pokemon.data.sprites?.front_default || 'https://media1.tenor.com/m/Tya2Q6TPVXQAAAAC/slowpoke-thinking.gif'} alt={`The pokemon ${pokemon.name} in their default front sprite`}/>
